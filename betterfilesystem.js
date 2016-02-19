77d88a3ef6bdf0f8302b4252b823cf475c4adcb7
Daniel Herr <DanielHerr@users.noreply.github.com>
2016-02-19 10:46:21 -0500

    Update betterfilesystem.js

--- betterfilesystem.js
+++ betterfilesystem.js
@@ -9,45 +9,45 @@ function betterfilesystem(...entries) {
     return(new Promise(function(resolve, reject) {
      file.file(function(file) {
       let reader = new FileReader()
-      reader.on("load", function() {
+      reader.addEventListener("load", function() {
        resolve(reader.result)
       })
-      reader.on("error", reject)
+      reader.addEventListener("error", reject)
       reader["readAs" + type](file)
    }, reject) })) }
    entry.write = function(data) {
     let file = this
     return(new Promise(function(resolve, reject) {
      file.createWriter(function(writer) {
-      writer.on("write", function() {
+      writer.addEventListener("write", function() {
        file.createWriter(function(writer) {
-        writer.on("write", resolve)
-        writer.on("error", reject)
+        writer.addEventListener("write", resolve)
+        writer.addEventListener("error", reject)
         writer.write(data)
       }) })
-      writer.on("error", reject)
+      writer.addEventListener("error", reject)
       writer.truncate(0)
      }, reject)
    })) }
    entry.remove = promisify(entry.remove)
   } else {
    entry.file = function(path, options) {
-    return(new Promise(enableawait(function*(resolve, reject) {
+    return(new Promise(async(function*(resolve, reject) {
      let file = yield(promisify(entry.getFile)(path, options))
      resolve(betterfilesystem(file))
    }))) }
    entry.folder = function(path, options) {
-    return(new Promise(enableawait(function*(resolve, reject) {
+    return(new Promise(async(function*(resolve, reject) {
      let folder = yield(promisify(entry.getDirectory)(path, options))
      resolve(betterfilesystem(folder))
    }))) }
    entry.remove = promisify(entry.removeRecursively)
    entry.read = function(recursive) {
     let folder = this
-    return(new Promise(enableawait(function*(resolve, reject) {
+    return(new Promise(async(function*(resolve, reject) {
      let contents = []
      function reading(reader) {
-      return(new Promise(enableawait(function*(resolve, reject) {
+      return(new Promise(async(function*(resolve, reject) {
        let entries = yield(reader.readsome())
        if(recursive != false) {
         for(let entry of entries) {
@@ -66,6 +66,9 @@ function betterfilesystem(...entries) {
      reader.readsome = promisify(reader.readEntries)
      yield(reading(reader))
      contents = betterfilesystem(...contents)
+     if(Array.isArray(contents) == false) {
+      contents = [ contents ]
+     }
      resolve(contents)
   }))) } }
   entry.parent = promisify(entry.getParent)
@@ -73,11 +76,11 @@ function betterfilesystem(...entries) {
   entry.copy = promisify(entry.copyTo)
   entry.metadata = promisify(entry.getMetadata)
   entry.rename = function(name) {
-   return(new Promise(enableawait(function*(resolve, reject) {
+   return(new Promise(async(function*(resolve, reject) {
     resolve(entry.move((yield(entry.parent())), name))
   }))) }
   entry.size = function() {
-   return(new Promise(enableawait(function*(resolve, reject) {
+   return(new Promise(async(function*(resolve, reject) {
     if(entry.isFile) {
      resolve((yield(entry.metadata())).size)
     } else {
@@ -88,8 +91,7 @@ function betterfilesystem(...entries) {
        size = size + (yield(entry.size()))
      } }
      resolve(size)
-  } }))) }
- }
+ } }))) } }
  if(entries.length == 1) {
   entries = entries[0]
  }
