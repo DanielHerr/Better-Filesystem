72646e425a23c5881336e356673c64de59c2df15
Daniel Herr <DanielHerr@users.noreply.github.com>
2016-03-15 14:29:32 -0400

    Update betterfilesystem.js

--- betterfilesystem.js
+++ betterfilesystem.js
@@ -30,67 +30,65 @@ function betterfilesystem(...entries) {
    })) }
    entry.remove = promisify(entry.remove)
   } else {
-   entry.file = function(path, options) {
-    return(new Promise(async(function*(resolve, reject) {
-     let file = yield(promisify(entry.getFile)(path, options))
-     resolve(betterfilesystem(file))
-   }))) }
-   entry.folder = function(path, options) {
-    return(new Promise(async(function*(resolve, reject) {
-     let folder = yield(promisify(entry.getDirectory)(path, options))
-     resolve(betterfilesystem(folder))
-   }))) }
+   entry.file = async(function*(path, options = {}) {
+    let file = yield(promisify(entry.getFile)(path, options))
+    return(betterfilesystem(file))
+   })
+   entry.folder = async(function*(path, options = {}) {
+    let folder = yield(promisify(entry.getDirectory)(path, options))
+    resolve(betterfilesystem(folder))
+   })
    entry.remove = promisify(entry.removeRecursively)
-   entry.read = function(recursive = true) {
+   entry.read = async(function*(recursive = true) {
     let folder = this
-    return(new Promise(async(function*(resolve, reject) {
-     let contents = []
-     function reading(reader) {
-      return(new Promise(async(function*(resolve, reject) {
-       let entries = yield(reader.readsome())
-       if(recursive) {
-        for(let entry of entries) {
-         if(entry.isDirectory) {
-          let reader = entry.createReader()
-          reader.readsome = promisify(reader.readEntries)
-          yield(reading(reader))
-       } } }
-       if(entries.length) {
-        contents.push(...entries)
+    let contents = []
+    var reading = async(function*(reader) {
+     let entries = yield(reader.readsome())
+     if(recursive) {
+      for(let entry of entries) {
+       if(entry.isDirectory) {
+        let reader = entry.createReader()
+        reader.readsome = promisify(reader.readEntries)
         yield(reading(reader))
-       }
-       resolve()
-     }))) }
-     let reader = folder.createReader()
-     reader.readsome = promisify(reader.readEntries)
-     yield(reading(reader))
-     contents = betterfilesystem(...contents)
-     if(Array.isArray(contents) == false) {
-      contents = [ contents ]
-     }
-     resolve(contents)
-  }))) } }
+     } } }
+     if(entries.length) {
+      contents.push(...entries)
+      yield(reading(reader))
+    } })
+    let reader = folder.createReader()
+    reader.readsome = promisify(reader.readEntries)
+    yield(reading(reader))
+    contents = betterfilesystem(...contents)
+    if(Array.isArray(contents) == false) {
+     contents = [ contents ]
+    }
+    return(contents)
+  }) }
   entry.parent = promisify(entry.getParent)
   entry.move = promisify(entry.moveTo)
   entry.copy = promisify(entry.copyTo)
+  entry.rename = async(function*(name) {
+   let parent = yield(entry.parent())
+   return(entry.move(parent, name))
+  })
   entry.metadata = promisify(entry.getMetadata)
-  entry.rename = function(name) {
-   return(new Promise(async(function*(resolve, reject) {
-    resolve(entry.move((yield(entry.parent())), name))
-  }))) }
-  entry.size = function() {
-   return(new Promise(async(function*(resolve, reject) {
-    if(entry.isFile) {
-     resolve((yield(entry.metadata())).size)
-    } else {
-     let size = 0
-     let contents = yield(entry.read())
-     for(let entry of contents) {
-      if(entry.isFile) {
-       size = size + (yield(entry.size()))
-     } }
-     resolve(size)
- } }))) } }
+  entry.date = async(function*() {
+   let metadata = yield(entry.metadata())
+   return(metadata.modificationTime)
+  })
+  entry.size = async(function*() {
+   if(entry.isFile) {
+    let metadata = yield(entry.metadata())
+    return(metadata.size)
+   } else {
+    let size = 0
+    let contents = yield(entry.read())
+    for(let entry of contents) {
+     if(entry.isFile) {
+      size = size + (yield(entry.size()))
+    } }
+    return(size)
+ } }) }
  if(entries.length == 1) {
   entries = entries[0]
  }
